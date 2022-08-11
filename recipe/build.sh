147752b3bcf3a7070e6f54e35c907b064080cfae
Isuru Fernando <isuruf@gmail.com>
2022-08-11 11:33:32 -0500

    Fix location

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* . || true
+cp $BUILD_PREFIX/share/gnuconfig/config.* build-aux/ || true
 
 ./configure --prefix=$PREFIX --libdir=$PREFIX/lib --disable-static
 make -j${CPU_COUNT}
