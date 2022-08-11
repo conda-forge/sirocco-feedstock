a0c5a42962cf79022a4a7218cceb414156be9c57
Isuru Fernando <isuruf@gmail.com>
2022-08-11 11:14:54 -0500

    update config.sub

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* . || true
+
 autoreconf --install
 ./configure --prefix=$PREFIX --libdir=$PREFIX/lib --disable-static
 make -j${CPU_COUNT}
 make install
-make check
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != 1 || "$CROSSCOMPILING_EMULATOR" != "" ]]; then
+  make check
+fi
