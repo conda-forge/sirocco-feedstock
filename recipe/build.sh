e07fc0deb526be24a7c8acbf05dadd0bd0943f78
Isuru Fernando <isuruf@gmail.com>
2022-08-11 11:25:09 -0500

    No autoreconf

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,6 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* . || true
 
-autoreconf --install
 ./configure --prefix=$PREFIX --libdir=$PREFIX/lib --disable-static
 make -j${CPU_COUNT}
 make install
